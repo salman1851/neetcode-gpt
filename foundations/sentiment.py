25ebd47192433ab5db297f41d4fd61d209a1d35b
salman1851 <131760691+salman1851@users.noreply.github.com>
2026-08-25 18:27:40 +1000

    Implement Sentiment analysis (NLP 3/4)

---
+++ foundations/sentiment.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+from torchtyping import TensorType
+
+class Solution(nn.Module):
+    def __init__(self, vocabulary_size: int):
+        super().__init__()
+        torch.manual_seed(0)
+        # Layers: Embedding(vocabulary_size, 16) -> Linear(16, 1) -> Sigmoid
+        
+        self.embedding = nn.Embedding(vocabulary_size, 16)
+        self.linear = nn.Linear(16, 1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x: TensorType[int]) -> TensorType[float]:
+        # Hint: The embedding layer outputs a B, T, embed_dim tensor
+        # but you should average it into a B, embed_dim tensor before using the Linear layer
+
+        # Return a B, 1 tensor and round to 4 decimal places
+        embed = self.embedding(x)
+        embed_avg = torch.mean(embed, dim=1)
+        lin = self.linear(embed_avg)
+        sig = self.sigmoid(lin)
+        return torch.round(sig, decimals=4)
