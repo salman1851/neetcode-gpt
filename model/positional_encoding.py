289e26375a9c5f9d20a20ea4019982e9a3ece805
salman1851 <131760691+salman1851@users.noreply.github.com>
2026-09-18 11:40:27 +1000

    Update Positional encoding (NLP 4/4)

--- model/positional_encoding.py
+++ model/positional_encoding.py
@@ -4,12 +4,19 @@ from numpy.typing import NDArray
 
 class Solution:
     def get_positional_encoding(self, seq_len: int, d_model: int) -> NDArray[np.float64]:
+        # pe = np.zeros((seq_len, d_model))
+        # for p in range(seq_len):
+        #     for i in range(d_model):
+        #         denom = 10000**(2*(i//2)/d_model)
+        #         if (i % 2 == 0):
+        #             pe[p,i] = np.sin(p/denom)
+        #         else:
+        #             pe[p,i] = np.cos(p/denom)
+        
+        position = np.arange(seq_len)[:, None]
+        div_term = np.exp(np.arange(0, d_model, 2) * (-np.log(10000.0) / d_model))
         pe = np.zeros((seq_len, d_model))
-        for p in range(seq_len):
-            for i in range(d_model):
-                denom = 10000**(2*(i//2)/d_model)
-                if (i % 2 == 0):
-                    pe[p,i] = np.sin(p/denom)
-                else:
-                    pe[p,i] = np.cos(p/denom)
+        pe[:, 0::2] = np.sin(position * div_term)
+        pe[:, 1::2] = np.cos(position * div_term)        
+
         return (np.round(pe, 5))
